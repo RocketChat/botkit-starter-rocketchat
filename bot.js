072fcefc8bac7a7ae71795fd106cf35c352d9ea0
ArthurTemporim <arthurrtl@gmail.com>
2018-05-24 21:27:23 -0300

    Add initial configuration of starter.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

---
+++ bot.js
@@ -0,0 +1,50 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+var env = require('node-env-file');
+env(__dirname + '/.env');
+
+
+var Botkit = require('botkit');
+var debug = require('debug')('botkit:main');
+
+var bot_options = { 
+    studio_token: process.env.studio_token,
+    studio_command_uri: process.env.studio_command_uri,
+    studio_stats_uri: process.env.studio_command_uri,
+    replyWithTyping: true,
+};
+
+// Create the Botkit controller, which controls all instances of the bot.
+var controller = Botkit.rocketchatbot(bot_options);
+
+// enable advanced botkit studio metrics
+require('botkit-studio-metrics')(controller);
+
+var normalizedPath = require("path").join(__dirname, "skills");
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  require("./skills/" + file)(controller);
+});
+
+
+// This captures and evaluates any message sent to the bot as a DM
+// or sent to the bot in the form "@bot message" and passes it to
+// Botkit Studio to evaluate for trigger words and patterns.
+// If a trigger is matched, the conversation will automatically fire!
+// You can tie into the execution of the script using the functions
+// controller.studio.before, controller.studio.after and controller.studio.validate
+
+function usage_tip() {
+    console.log('~~~~~~~~~~');
+    console.log('Botkit Studio Starter Kit');
+    console.log('Execute your bot application like this:');
+    console.log('page_token=<MY PAGE TOKEN> verify_token=<MY VERIFY TOKEN> studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
+    console.log('Get Facebook token here: https://developers.facebook.com/docs/messenger-platform/implementation')
+    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
+    console.log('~~~~~~~~~~');
+}
